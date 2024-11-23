14e01f49f26b74dd3a353296221a47eefc4ea089
Vlad Bulahov <dasistperfektosss@gmail.com>
2024-11-23 22:49:53 +0000

    add dependencies

---
+++ src/main/java/com/raxrot/sbstudentmanagementsystem/SbStudentManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.raxrot.sbstudentmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbStudentManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbStudentManagementSystemApplication.class, args);
+    }
+
+}
