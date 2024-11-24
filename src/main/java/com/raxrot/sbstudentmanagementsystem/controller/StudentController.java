04b6f1b2c9e837f810b771a4adba23231cd8cf5b
Vlad Bulahov <dasistperfektosss@gmail.com>
2024-11-24 15:58:53 +0000

    add CreateStudent Handler

--- src/main/java/com/raxrot/sbstudentmanagementsystem/controller/StudentController.java
+++ src/main/java/com/raxrot/sbstudentmanagementsystem/controller/StudentController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -33,5 +35,12 @@ public class StudentController {
         StudentDto studentDto = new StudentDto();
         model.addAttribute("student", studentDto);
         return "create_student";
+    }
+
+    //handler method to save student from submitForm
+    @PostMapping("/students")
+    public String saveStudent(@ModelAttribute("student") StudentDto studentDto) {
+        studentService.createStudent(studentDto);
+        return "redirect:/students";
     }
 }
